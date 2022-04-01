d13f30f7f92796f677f11346bf007aa817cd08d7
Alexey <notlexa>
2022-04-01 18:41:01 +0300

    Можно посмотреть немного информации об игре

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 #from orm import db_session
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcdef35q3pouihmglgtjcdkoiyug'
@@ -19,7 +20,35 @@ def page_search():
 
 @app.route('/game')
 def page_game():
-    return render_template('game.html')
+    steamid = request.args.get('steamid')
+    if steamid is not None: # указан steamid игры, который нужно найти
+        for i in range(len(games)):
+            if games[i]['url_info']['id'] == steamid:
+                break # найден steamid
+        else: # не найден steamid ---------------------------.
+            return redirect('/', 301) # возвращаемся домой   |
+        # сюда мы выходим в случае break   <-----------------'
+        id = i
+    else: # не указан steamid
+        id = request.args.get('gameid', type=int) # указан gameid игры
+        if id is None: # даже id нет
+            redirect('/', 301) # возвращаемся домой
+    gamedata = games[id]
+    return render_template('game.html',
+                           name=gamedata['name'], # единственный параметр, который есть у всех элементов games
+                           desc=gamedata.get('full_desc', {'desc':'<Нет описания>'})['desc'],
+                           date=gamedata.get('date', '<Даты выхода нет>'),
+                           dev=gamedata.get('developer', '<Разработчик не указан>'),
+                           cents=gamedata.get('price', '<Цена не указана>'),
+                           steamid=gamedata.get('url_info', {}).get('id', '<Не указан SteamID>'),
+                           steamlink=gamedata.get('ulr_info', {}).get('url', 'no link'),
+                           imgurl=gamedata['img_url'] # ладно, может не единственный...
+                           # если steamlink == no link, то ссылку не создавать (таких случаев кстати не должно быть)
+                           )
 
 if __name__ == '__main__':
+    print('Загружается games.json...')
+    with open('db/games.json', 'r') as f:
+        games = json.load(f)
+    print('Загрузка завершена! Запускаем приложение...')
     app.run(port=8080, host='127.0.0.1')
