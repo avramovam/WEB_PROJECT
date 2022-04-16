a73b60cf23c34f1e7cd5ce9ad8a46776eb4503e5
Alexey <NotLexa>
2022-04-16 22:48:35 +0300

    Сортировка частей кода по регионам (см заголовки регионов)

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+#region [ОБРАБОТКА ОШИБОК]
+@app.errorhandler(404)
+def e404(error):
+    return render_template('404.html'), error.code
+#endregion
+
+#region [ЛОГИН И РЕГИСТРАЦИЯ]
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -20,7 +27,6 @@ login_manager.init_app(app)
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,11 +40,37 @@ def login():
         return render_template('login.html', message="Wrong login or password", form=form)
     return render_template('login.html', title='Authorization', form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Register', form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register', form=form,
+                                   message="This user already exists")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
-@app.errorhandler(404)
-def e404(error):
-    return render_template('404.html'), error.code
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+#endregion
 
+#region [ОСНОВНЫЕ СТРАНИЦЫ]
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
@@ -46,12 +78,6 @@ def index():
     names = {name.id: (name.surname, name.name) for name in users}
     return render_template("index.html", names=names, title='Games')
 
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
 @app.route('/search')
 def page_search():
     a_name = request.args.get('name')
@@ -82,29 +108,6 @@ def page_search():
     return render_template('search.html', games=filtered[startwith:startwith+pagesize],
                            link=recursive_link, page=a_page, maxpage=floor((len(filtered)-1)/pagesize))
 
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Register', form=form,
-                                   message="Passwords don't match")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Register', form=form,
-                                   message="This user already exists")
-        user = User(
-            name=form.name.data,
-            surname=form.surname.data,
-            age=form.age.data,
-            email=form.email.data,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
-
 @app.route('/game')
 def page_game():
     steamid = request.args.get('steamid')
@@ -132,6 +135,7 @@ def page_game():
                            imgurl=gamedata['img_url'] # ладно, может не единственный...
                            # если steamlink == no link, то ссылку не создавать (таких случаев кстати не должно быть)
                            )
+#endregion
 
 if __name__ == '__main__':
     db_session.global_init("db/users.sqlite")
